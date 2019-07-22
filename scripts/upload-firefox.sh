d3acabb4a9d00700509eb369bff5312bc302bd8e
Poy Chang <poypost@gmail.com>
2019-07-22 22:16:37 +0800

    try to fix CD process of AMO

--- scripts/upload-firefox.sh
+++ scripts/upload-firefox.sh
@@ -3,4 +3,5 @@
 echo "----------------------------------------------------------------------"
 echo "Upload Github Dark Theme Firefox Add-ons to https://addons.mozilla.org"
 echo "----------------------------------------------------------------------"
-npx web-ext sign --channel listed --source-dir ./dist-firefox --artifacts-dir ./ --api-key=$AMO_JWT_ISSUER --api-secret=$AMO_JWT_SECRET
+
+npx shipit firefox ./dist-firefox
