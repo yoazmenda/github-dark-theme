5da7eb8d55045d226b527b1c3b9d3bc2da526cdd
Poy Chang <poypost@gmail.com>
2019-10-13 07:09:23 +0800

    version+ and update exclude url list

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ const config = {
         nameOfPrefersScheme: 'usePrefersScheme',
     },
     defaultDomainList: ['github.com'],
-    excludeUrlList: ['github.com/explore', 'github.com/marketplace'],
+    excludeUrlList: ['github.com/marketplace'],
     uninstallQuestionnaire:
         'https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__c9PL4pURTlJSFdFUzZUSzNBNUs4N0JaQlhEUkRBTy4u',
 };
